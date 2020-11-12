2deadd7ee0e514e455fdbe6d5e040afe8fae7c33
Sergio Neves <sernevess@gmail.com>
2020-11-12 18:11:46 +0000

    Changes to SW initalisation methods

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import "./style/index.css";
 import App from "./components/app.tsx";
+import { initialiseSW } from "./utils";
+
+initialiseSW()
 
 export default App;
---
+++ src/sw.ts
@@ -0,0 +1,13 @@
+/*
+// Defaul configs for sw using Google Workbox, might use it later
+
+import { getFiles, setupPrecaching, setupRouting } from 'preact-cli/sw/';
+
+
+setupRouting();
+setupPrecaching(getFiles());
+
+*/
+self.addEventListener("install", () => {
+    console.log("hello from SW!");
+});
