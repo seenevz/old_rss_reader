48e75ded6b666a0e68dfc2e7f01fdee5732fc591
Sergio Neves <sernevess@gmail.com>
2020-11-24 16:05:00 +0000

    Added empty home placeholder with respective styles

--- src/routes/home/index.tsx
+++ src/routes/home/index.tsx
@@ -1,7 +1,12 @@
 import { FunctionalComponent, h } from "preact";
+import emptyImg from "../../assets/images/background_empty.jpg";
 
 export default function Home() {
-  return <main>
-    this is the main page
+  return <main class='section is-large'>
+    <figure class='image is-square mt-6'>
+      <img src={emptyImg} style="transform: rotate(90deg)"/>
+    </figure>
+    <h2 class='title is-3 has-text-centered has-text-grey-light'>It's a bit empty here...</h2>
+    <h3 class='subtitle is-5 has-text-centered has-text-grey-lighter'>Add some feeds to start</h3>
   </main>
 }
