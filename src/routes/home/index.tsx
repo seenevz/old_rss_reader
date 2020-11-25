eff1ae5eb28e3f78e1193ebd2570c4b58f27bd7c
Sergio Neves <sernevess@gmail.com>
2020-11-25 15:01:52 +0000

    Created feeds container component

--- src/routes/home/index.tsx
+++ src/routes/home/index.tsx
@@ -1,12 +1,14 @@
-import { FunctionalComponent, h } from "preact";
-import emptyImg from "../../assets/images/background_empty.jpg";
+import { h } from "preact";
+import { useState } from "preact/hooks";
+import EmptyFeed from "../../components/emptyFeed";
+import FeedsContainer from "../../components/feedsContainer";
 
 export default function Home() {
-  return <main class='section is-large'>
-    <figure class='image is-square mt-6'>
-      <img src={emptyImg} style="transform: rotate(90deg)"/>
-    </figure>
-    <h2 class='title is-3 has-text-centered has-text-grey-light'>It's a bit empty here...</h2>
-    <h3 class='subtitle is-5 has-text-centered has-text-grey-lighter'>Add some feeds to start</h3>
-  </main>
+    const [feeds, setFeeds] = useState([]);
+
+    return (
+        <main class="section is-large">
+            {feeds.length > 0 ? <FeedsContainer feeds={feeds} /> : <EmptyFeed />}
+        </main>
+    );
 }
