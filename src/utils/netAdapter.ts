5db9b99ecb3762e68d514cc1265d7d69b8e560c1
Sergio Neves <sernevess@gmail.com>
2020-11-30 16:37:31 +0000

    rewrite fetch to include proxy by default

--- src/utils/netAdapter.ts
+++ src/utils/netAdapter.ts
@@ -1,5 +1,24 @@
 import feeds from "../testFeeds";
+import { proxyUrl } from "../constants";
 
-export function getFeeds() {
+type feedsList = { feeds: { url: string }[] };
+
+const myFetch = async (url: string): Promise<string> => {
+    return await (
+        await fetch(proxyUrl + url, {
+            headers: new Headers([
+                ["x-auth-token", process.env.TOKEN as string]
+            ])
+        })
+    ).text();
+};
+
+export function getFeeds(): Promise<feedsList> {
     return Promise.resolve(feeds);
+}
+
+export async function getFeedXML(url: string) {
+    const resp = await myFetch(url);
+
+    console.log(resp);
 }
