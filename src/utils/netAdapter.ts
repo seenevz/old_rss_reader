b5a3eb2e56f9740fd9f7532d87c32a423e5fcb1f
Sergio Neves <sernevess@gmail.com>
2020-12-03 12:23:05 +0000

    rewrite method to fetch xml data

--- src/utils/netAdapter.ts
+++ src/utils/netAdapter.ts
@@ -1,14 +1,13 @@
 import feedsJSON from "../testFeeds";
 import { proxyUrl } from "../constants";
+import { process } from "../env";
 
 export type feedsListType = { url: string }[];
 
 const myFetch = async (url: string): Promise<string> => {
     return await (
         await fetch(proxyUrl + url, {
-            headers: new Headers([
-                ["x-auth-token", process.env.TOKEN as string]
-            ])
+            headers: new Headers([["x-auth-token", process.env.TOKEN]])
         })
     ).text();
 };
@@ -17,8 +16,6 @@ export function getFeeds(): Promise<feedsListType> {
     return Promise.resolve(feedsJSON.feeds);
 }
 
-export async function getFeedXML(url: string) {
-    const resp = await myFetch(url);
-
-    console.log(resp);
+export async function getFeedXML(url: string): Promise<string> {
+    return await myFetch(url);
 }
