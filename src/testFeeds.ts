22f7f9652781431cea80ff09ff5ef4cae44069f1
Sergio Neves <sernevess@gmail.com>
2020-12-03 12:18:23 +0000

    fixed typo in one test url

--- src/testFeeds.ts
+++ src/testFeeds.ts
@@ -1,7 +1,7 @@
 export default {
     feeds: [
         { url: "https://lifehacker.com/rss" },
-        { url: "http://www.reddit.com/.rss" },
+        { url: "https://www.reddit.com/.rss" },
         { url: "https://feeds.bbci.co.uk/news/rss.xml" }
     ]
 };
