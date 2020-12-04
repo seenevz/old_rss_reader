c551562dbbcaf4091aab8411add6c8a5e4a47425
Sergio Neves <sernevess@gmail.com>
2020-12-04 23:20:10 +0000

    created nested list of item for each feed

--- src/components/feedsContainerRow/index.tsx
+++ src/components/feedsContainerRow/index.tsx
@@ -1,5 +1,7 @@
 import { h } from "preact";
+import { useEffect, useState } from "preact/hooks";
 import { FeedType } from "../feedsContainer";
+import { getFeedXML, getDataFromXML, ParsedFeed } from "../../utils";
 
 type FeedsContainerRowProps = {
     feed: FeedType;
@@ -8,5 +10,41 @@ type FeedsContainerRowProps = {
 export default function FeedsContainerRow({
     feed: { url }
 }: FeedsContainerRowProps) {
-    return <article class="media">{url}</article>;
+    const [parsedFeed, setParsedFeed] = useState({} as ParsedFeed);
+    const [expandList, setExpandList] = useState(false);
+
+    useEffect(() => {
+        (async (): Promise<void> => {
+            const feedString = await getFeedXML(url);
+            setParsedFeed(getDataFromXML(feedString));
+        })();
+    }, [url]);
+
+    const toggleItemList = (): void => setExpandList(val => !val);
+
+    return (
+        <article class="media">
+            <div class="media-content" onClick={toggleItemList}>
+                <div class="content">
+                    <h2 class="title is-6">{parsedFeed?.feedInfo?.title}</h2>
+                    <h3 class="subtitle is-6">
+                        {parsedFeed?.feedInfo?.description}
+                    </h3>
+                </div>
+                {expandList &&
+                    parsedFeed.feedItems.map(item => {
+                        return (
+                            <article key={Math.random()} class="media">
+                                <div class="media-left">
+                                    <p class="image is-16x16"></p>
+                                </div>
+                                <div class="media-content">
+                                    <div class="content">{item?.title}</div>
+                                </div>
+                            </article>
+                        );
+                    })}
+            </div>
+        </article>
+    );
 }
